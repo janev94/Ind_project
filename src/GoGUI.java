b39b5e6c26f83b5c3c94d092c4c67c40204f52b0
janev94 <misho.janew@gmail.com>
2016-10-18 20:13:50 +0100

    GUI drawing with overlaps

--- src/GoGUI.java
+++ src/GoGUI.java
@@ -13,29 +13,37 @@ import javafx.stage.Stage;
 
 public class GoGUI extends Application {
 
-	int boardWidth = 5;
+	int boardWidth = 10;
 	int boardHeight = 5;
+
+	private boolean turn = true;
+
 	
 	@Override
 	public void start(Stage stage) {
 		
 		List<Circle> positions = new ArrayList<>();
 		List<Line> lines = new ArrayList<>();
+		
+		
+		//consider doing it with squares (enables filling etc)
+		for(int y=0; y < boardHeight; y++) {
+			Line xLine = new Line(50, (y+1) * 40
+					, boardWidth * 40 + 10, (y+1) * 40);
+			lines.add(xLine);
+		}
+		
+		for(int y=0; y < boardWidth; y++) {
+			
+			Line yLine = new Line((y+1) * 40 + 10, 40,  
+					(y+1) * 40 + 10, boardHeight * 40);
+			lines.add(yLine);
+		
+		}
+	
+		
 		for(int y=0; y < boardHeight; y++) {
 			for(int x=0; x < boardWidth; x++) {
-
-				if(x < boardWidth - 1) {
-					Line xLine = new Line((x + 1 ) * 40 + 20, (y+1) * 40
-										, (x+2) * 40, (y+1) * 40);
-					lines.add(xLine);
-				}
-
-				if( y < boardHeight - 1) {
-					Line yLine = new Line((x + 1) * 40 + 10, (y+1) * 40 + 10 
-										,(x + 1) * 40 + 10, (y+2) * 40 - 10);
-					lines.add(yLine);
-				}
-				
 				Circle circ = new Circle((x+1) * 40 + 10, (y+1) * 40,
 						10, Color.TRANSPARENT);
 				circ.setStrokeWidth(1);
@@ -43,7 +51,8 @@ public class GoGUI extends Application {
 				
 				circ.setOnMouseClicked(e -> { 
 					Circle c = (Circle) e.getSource();
-					c.setFill(Color.BLACK);
+					c.setFill(turn?Color.BLACK: Color.WHITESMOKE);
+					turn = !turn;
 				});
 				
 				positions.add(circ);
@@ -61,7 +70,7 @@ public class GoGUI extends Application {
         
         Scene scene = new Scene(root, 400, 300);
 
-        stage.setTitle("My JavaFX Application");
+        stage.setTitle("Go appl");
         stage.setScene(scene);
         stage.show();
     }
