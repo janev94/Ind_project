a7e4232742ea89e4375aed2fc64ddc50a97b23eb
janev94 <misho.janew@gmail.com>
2016-10-23 19:45:46 +0100

    GUI implemented with rectangles, magic numbers removed

--- src/GoGUI.java
+++ src/GoGUI.java
@@ -3,18 +3,23 @@ import java.util.List;
 
 import javafx.application.Application;
 import javafx.event.Event;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.CircleBuilder;
 import javafx.scene.shape.Line;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 public class GoGUI extends Application {
 
-	int boardWidth = 10;
+	int boardWidth = 9;
 	int boardHeight = 5;
+	
+	private int SQ_SIDE = 40;
+	private int RADIUS = 10;
 
 	private boolean turn = true;
 
@@ -24,34 +29,32 @@ public class GoGUI extends Application {
 		
 		List<Circle> positions = new ArrayList<>();
 		List<Line> lines = new ArrayList<>();
+		List<Rectangle> rects = new ArrayList<>();
 		
 		
-		//consider doing it with squares (enables filling etc)
-		for(int y=0; y < boardHeight; y++) {
-			Line xLine = new Line(50, (y+1) * 40
-					, boardWidth * 40 + 10, (y+1) * 40);
-			lines.add(xLine);
-		}
-		
-		for(int y=0; y < boardWidth; y++) {
-			
-			Line yLine = new Line((y+1) * 40 + 10, 40,  
-					(y+1) * 40 + 10, boardHeight * 40);
-			lines.add(yLine);
-		
+		for(int y=0;y < boardHeight - 1; y++) {
+			for( int x=0;x < boardWidth - 1; x++) {
+				Rectangle rect = new Rectangle(x * SQ_SIDE + (SQ_SIDE + RADIUS), y * SQ_SIDE + SQ_SIDE, SQ_SIDE, SQ_SIDE);
+				rect.setFill(Color.BURLYWOOD);
+				rect.setStrokeWidth(1);
+				rect.setStroke(Color.BLACK);
+				rect.toBack();
+				rects.add(rect);
+			}
 		}
 	
 		
 		for(int y=0; y < boardHeight; y++) {
 			for(int x=0; x < boardWidth; x++) {
-				Circle circ = new Circle((x+1) * 40 + 10, (y+1) * 40,
-						10, Color.TRANSPARENT);
+				Circle circ = new Circle((x+1) * SQ_SIDE + RADIUS, (y+1) * SQ_SIDE,
+						RADIUS, Color.TRANSPARENT);
 				circ.setStrokeWidth(1);
 				circ.setStroke(Color.BLACK);
 				
 				circ.setOnMouseClicked(e -> { 
 					Circle c = (Circle) e.getSource();
-					c.setFill(turn?Color.BLACK: Color.WHITESMOKE);
+					c.setFill(turn?Color.BLACK: Color.rgb(255, 0, 0, 1.0));
+					c.toFront();
 					turn = !turn;
 				});
 				
@@ -65,10 +68,11 @@ public class GoGUI extends Application {
         circ.setStrokeWidth(3);
         circ.setStroke(Color.BLACK);
         Group root = new Group();
+//        root.getChildren().addAll(positions);
+        //root.getChildren().addAll(lines);
+        root.getChildren().addAll(rects);
         root.getChildren().addAll(positions);
-        root.getChildren().addAll(lines);
-        
-        Scene scene = new Scene(root, 400, 300);
+        Scene scene = new Scene(root, SQ_SIDE * (boardWidth + 1), SQ_SIDE * (boardHeight + 1));
 
         stage.setTitle("Go appl");
         stage.setScene(scene);
