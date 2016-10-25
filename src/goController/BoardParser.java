2d23ea27a3aab1a82e11efa086a58ec19e084cb5
janev94 <misho.janew@gmail.com>
2016-10-25 23:56:00 +0100

    Parser abstraction created

---
+++ src/goController/BoardParser.java
@@ -0,0 +1,24 @@
+package goController;
+
+public abstract class BoardParser {
+
+	
+//	public static BoardParser createParser(String type) {
+//		
+//		return new BoardParserString();
+//	}
+	
+	public abstract Stone[][] parse();
+	
+	public Stone[][] setUpEmptyBoard(int width, int height){
+		Stone[][] stonePositions = new Stone[width][height];
+		
+		for(int x = 0; x < width; x++){
+			for(int y = 0; y < height; y++) {
+				stonePositions[x][y] = new Stone(StoneOwner.EMPTY);
+			}
+		}
+		
+		return stonePositions;
+	}
+}
---
+++ src/goController/DummyParser.java
@@ -0,0 +1,10 @@
+package goController;
+
+public class DummyParser extends BoardParser {
+
+	@Override
+	public Stone[][] parse() {
+
+		return null;
+	}
+}
