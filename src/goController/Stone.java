ace3905c569d5ee4ec9494131c33572705a69aee
janev94 <misho.janew@gmail.com>
2016-11-29 19:14:37 +0000

    Created copy constructor for Stone #3 and added equals
    Modified toString, added equals and hashcode functionallity

--- src/goController/Stone.java
+++ src/goController/Stone.java
@@ -13,6 +13,12 @@ public class Stone {
 		this.col = col;
 	}
 	
+	public Stone(Stone stone) {
+		this.owner = stone.owner;
+		this.row = stone.row;
+		this.col = stone.col;
+	}
+
 	public StoneOwner getOwner() {
 		return owner;
 	}
@@ -25,8 +31,42 @@ public class Stone {
 		return col;
 	}
 	
+	public void setOwner(StoneOwner owner) {
+		this.owner = owner;
+	}
+	
 	@Override
 	public String toString() {
-		return "Stone owned by: " + owner.name();
+		return "Stone(" + row + ", " + col + ") owned by: " + owner.name();
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + col;
+		result = prime * result + ((owner == null) ? 0 : owner.hashCode());
+		result = prime * result + row;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Stone other = (Stone) obj;
+		if (col != other.col)
+			return false;
+		if (owner != other.owner)
+			return false;
+		if (row != other.row)
+			return false;
+		return true;
+	}
+	
+	
 }
